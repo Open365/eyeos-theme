e525b964055f2f9e6bb6b765988e94c705729dfb
Carlos Campderrós <carlos.campderros@eyeos.com>
2016-06-10 12:47:47 +0200

    set git user
    it is needed to be able to commit the version bump, or else git
    complains about unknown user

--- commit-stage.sh
+++ commit-stage.sh
@@ -4,4 +4,10 @@ set -u
 
 npm install
 npm install -g grunt-cli istanbul bower
+# set git user/config if not previously set
+if ! git config user.name > /dev/null
+then
+	git config --global user.name jenkins
+	git config --global user.email jenkins@eyeos.com
+fi
 grunt commit-stage
